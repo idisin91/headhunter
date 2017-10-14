4b33d96b61186cd84c22db5486722ca355deb6d6
idisin91 <idisin91@gmail.com>
2017-10-14 23:37:30 +0300

    Initial commit

---
+++ headhunterApp/CardsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CardsViewController.swift
+//  headhunterApp
+//
+//  Created by Никита Скрынников on 14.10.17.
+//  Copyright © 2017 ForMe. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
